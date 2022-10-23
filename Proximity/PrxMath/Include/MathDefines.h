294f6900930b149a037102ba20e616f252a029ce
Arnav Mehta <arnavdatt@gmail.com>
2022-10-23 14:59:19 +0100

    Wrapped typedefs in namespace

--- Proximity/PrxMath/Include/MathDefines.h
+++ Proximity/PrxMath/Include/MathDefines.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <cstdint>
 
-using U64 = uint64_t;
-using U32 = uint32_t;  // This is the standard int
-using U16 = uint16_t;
-using U8  = uint8_t;
+namespace Proximity::Math
+{
+	using U64 = uint64_t;
+	using U32 = uint32_t;  // This is the standard uint
+	using U16 = uint16_t;
+	using U8 = uint8_t;
 
-using I64 = int64_t;
-using I32 = int32_t;
-using I16 = int16_t;
-using I8  = int8_t;
+	using I64 = int64_t;
+	using I32 = int32_t;  // This is the standard int
+	using I16 = int16_t;
+	using I8 = int8_t;
 
-using F32 = float;
+	using F32 = float;
 
 #define XMVECTOR_LOAD3(varName, value) DX::XMVECTOR varName = DX::XMLoadFloat3(value)
 #define XMVECTOR_LOAD4(varName, value) DX::XMVECTOR varName = DX::XMLoadFloat4(value)
-#define XMMATRIX_LOAD(varName, value)  DX::XMMATRIX varName = DX::XMLoadFloat4x4(value)
+#define XMMATRIX_LOAD(varName, value)  DX::XMMATRIX varName = DX::XMLoadFloat4x4(value) 
+}
