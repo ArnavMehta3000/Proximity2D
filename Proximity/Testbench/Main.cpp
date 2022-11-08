e639b6c51d037dfef916faca47308c687c9cd3cf
Arnav Mehta <arnavdatt@gmail.com>
2022-11-08 13:02:27 +0000

    Update

--- Proximity/Testbench/Main.cpp
+++ Proximity/Testbench/Main.cpp
@@ -1,7 +1,7 @@
+#include "pch.h"
 #include"../PrxEngine/Include/PrxEngine.h"
 #include "../PrxEngine/Include/PrxMath.h"
 #include "../PrxEngine/Include/PrxUtils.h"
-#include <iostream>
 using namespace Proximity::Math;
 using namespace Proximity::Utils;
 
@@ -17,17 +17,16 @@ public:
 	{
 
 	}
+
+	static void TestFunction(int num)
+	{
+		int x = num;
+	}
 };
+
+
 	
 int main()
 {
-	ServiceLocator* sl = new ServiceLocator();
-	sl->RegisterService<TestService>();
-	std::cout << sl->ResolveService<TestService>()->ServiceID() << std::endl;
-	sl->UnRegisterService<TestService>();
-	sl->RegisterService<TestService>();
-	std::cout << sl->ResolveService<TestService>()->ServiceID() << std::endl;
-
-	
 	return 0;
 }
