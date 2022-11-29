dbada14890f0467e289512eb26bb1e1f63a646f6
Arnav Mehta <arnavdatt@gmail.com>
2022-11-29 01:03:52 +0000

    Added ECS files

--- Proximity/PrxEngine/Include/PrxEngine.h
+++ Proximity/PrxEngine/Include/PrxEngine.h
@@ -6,3 +6,7 @@
 #include "../Include/Input/InputSystem.h"
 #include "Engine/EngineGlobals.h"
 #include "Engine/Application.h"
+#include "Engine/World/Scene.h"
+#include "Engine/World/Entity.h"
+#include "Engine/World/World.h"
+#include "Engine/Components/Components.h"
