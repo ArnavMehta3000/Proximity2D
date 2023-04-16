4ade37e0d2e307037cc7ae71b38b0005bb9b3455
Arnav Mehta <arnavdatt@gmail.com>
2023-04-16 20:07:01 +0100

    Added build project bat file

---
+++ Build.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Updating submodules
+
+cd "Proximity\PrxEngine\External\yaml-cpp\"
+git submodule update --recursive
+
+echo Building yaml-cpp
+
+mkdir build
+cd build
+
+echo Added [yaml-cpp] 'build' directory
+
+echo Using CMAKE to build [yaml-cpp]
+
+cmake ..
+
+pause
